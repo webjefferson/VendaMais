17b7cf6276debfda207e5b225bc96512108082cd
Jefferson Rodrigues <webjefferson@gmail.com>
2015-07-22 10:51:33 -0300

    Update

--- vdmAdmin/Properties/Settings.Designer.cs
+++ vdmAdmin/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace vdmAdmin.Properties
-{
-
-
+namespace vdmAdmin.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=JEFFERSON-PC\\SQLEXPRESS;Initial Catalog=VendaMais;Integrated Security" +
+            "=True")]
+        public string VendaMaisConnectionString {
+            get {
+                return ((string)(this["VendaMaisConnectionString"]));
             }
         }
     }
